c0b44b2b29884d48f2e5eab2bf19369abf9b6239
Gian Paolo Estacio <gianpaoloestacio5@gmail.com>
2022-02-22 16:49:58 +0800

    update

--- anykernel.sh
+++ anykernel.sh
@@ -4,13 +4,17 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=
+kernel.string=Gear Kernel for Samsung Galaxy M20 (m20lte)
 do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=1
-device.name1=
+device.name1=m20lte
+device.name2=M20
+device.name3=m20
+device.name4=M20LTE
+device.name5=
 supported.versions=
 supported.patchlevels=
 '; } # end properties
